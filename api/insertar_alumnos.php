d344979d20b386da84ed9749a511e8d30a610e85
aledzmx <alexisdzg2003@gmail.com>
2025-10-24 10:50:23 -0600

    Update insertar_alumnos.php

--- api/insertar_alumnos.php
+++ api/insertar_alumnos.php
@@ -1,34 +1,43 @@
 <?php
 
-header ("Access-Control-Allow-Origin: *");
-header ("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header ("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200); 
+    exit(); // Salir del script
+}
+
 header("Content-Type: application/json; charset=utf-8");
 
 if(isset($_GET["nombre"])){
 
-$nombre=$_GET["nombre"];
-$apellidos=$_GET["apellidos"];
+    $nombre = $_GET["nombre"];
+    $apellidos = $_GET["apellidos"];
 
-include("db.php");
+    include("db.php");
 
-$stmt = $conn->prepare("INSERT INTO estudiantes (nombre, apellidos) VALUES (?,?)");
-$stmt->bind_param("ss",$nombre,$apellidos);
+    $stmt = $conn->prepare("INSERT INTO estudiantes (nombre, apellidos) VALUES (?,?)");
+    $stmt->bind_param("ss", $nombre, $apellidos);
 
-if($stmt->execute()){
-    http_response_code(201);
-    echo json_encode([
-        "status"=>"success",
-        "menssage"=>"Datos insertados correctamente",
-        "insert_id"=>$stmt->insert_id
-    ]);
-}else{
-    http_response_code(500);
-    echo json_encode([
-        "status"=>"error",
-        "menssage"=>"Error al insertar los datos"
-    ]);
+    if($stmt->execute()){
+        http_response_code(201);
+        echo json_encode([
+            "status" => "success",
+            "menssage" => "Datos insertados correctamente",
+            "insert_id" => $stmt->insert_id
+        ]);
+    } else {
+        http_response_code(500);
+        echo json_encode([
+            "status" => "error",
+            "menssage" => "Error al insertar los datos"
+        ]);
+    }
+    
+    $stmt->close();
+    $conn->close();
 
-}
-
-}
+} 
+?>
